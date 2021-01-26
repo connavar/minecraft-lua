7b88d88d99b33024756b3df5e1492fe4c784be19
Kevin Stroud <g33k3n01d@gmail.com>
2021-01-26 15:48:10 +0200

    init

---
+++ placefloor.lua
@@ -0,0 +1,53 @@
+print("Starting")
+for j = 1, 30 do
+	print("Outer loop - column: " .. j)
+	for i = 1, 32 do
+		print("Inner loop - row: " .. i)
+		place()
+	end
+	turtle.turnRight()
+	turtle.turnRight()
+	print("Turning")
+	ok = turtle.forward()
+	if ok then
+		print("Turn success")
+		turtle.turnRight()
+	else
+		print("Turn fail")
+		return
+	end
+end
+
+function place()
+	turtle.digDown()
+	turtle.placeDown()
+	turtle.turnLeft()
+	ok = turtle.forward()
+
+	if ok then
+		print("Left Turn")
+		turtle.digDown()
+		turtle.placeDown()
+		turtle.turnRight()
+		turtle.turnRight()
+		turtle.forward()
+		ok = turtle.forward()
+	else
+		turtle.turnRight()
+		turtle.turnRight()
+		ok = turtle.forward()
+	end
+
+	if ok then
+		print("Turn Right")
+		turtle.digDown()
+		turtle.placeDown()
+		turtle.turnLeft()
+		turtle.turnLeft()
+		turtle.forward()
+		turtle.forward()
+		turtle.turnRight()
+	else
+		turtle.turnLeft()
+	end
+end
